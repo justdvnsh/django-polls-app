7b6f83e8e3ffe19cc740ae4d7b45192ed0183ce9
justdvnsh <justdvnsh2208@gmail.com>
2020-02-13 12:42:07 +0530

    added views and templates

--- pollsApp/views.py
+++ pollsApp/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Questions
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hey there ... You are at index of views pollsApp")
+    latest = Questions.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest': latest
+    }
+    return render(request, "pollsApp/index.html", context)
+
+def details(request, question_id):
+    question = get_object_or_404(Questions, pk=question_id)
+    return render(request, "pollsApp/detail.html", {'question': question})
+
+def results(request, question_id):
+    return HttpResponse("Hey there ... You are looking at the results of the %s question number."%question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Hey There ... You are at the voting page. Your uestions has %s votes"%question_id)
